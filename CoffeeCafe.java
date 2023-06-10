74eab735489fd97b247d56ce72b08a7f3f33059b
Gorakh Sawant <96041987+GorakhSawant@users.noreply.github.com>
2023-06-11 02:46:57 +0530

    Add files via upload

---
+++ CoffeeCafe.java
@@ -0,0 +1,14 @@
+
+package cafe;
+
+
+public class CoffeeCafe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
